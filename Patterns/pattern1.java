c3e9628a03507c90c9c2b84fcfa632ba81a720b9
suchitraguttula <suchitra@driftsync.in>
2025-11-12 11:11:17 +0530

    pattern 1

---
+++ Patterns/pattern1.java
@@ -0,0 +1,21 @@
+//\Problem Statement: Given an integer N, print the following pattern.
+import java.util.Scanner;
+public class Pattern1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int rows = scanner.nextInt();
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for(int i=1;i<rows;i++){
+            System.out.print("* ".repeat(i));
+        }
+        System.out.println();
+        scanner.close();
+    }
+}
